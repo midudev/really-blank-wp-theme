f22711dde4baedd9286e5b41de102dda1f9eec7a
Miguel Ángel <miguel@miduga.es>
2012-10-02 20:07:53 +0200

    jQuery 1.8.2 and added core style for WordPress

--- footer.php
+++ footer.php
@@ -4,6 +4,6 @@
 
 </section>
 
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
 
 <?php wp_footer(); ?>
