b6db6640b7b8f074389110997cb3852a1042c716
Miguel Ángel <miguel@miduga.es>
2013-02-07 14:49:36 +0100

    Update jQuery to 1.9 and fix for css class name

--- footer.php
+++ footer.php
@@ -4,6 +4,6 @@
 
 </section>
 
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
 
-<?php wp_footer(); ?>
+<?php wp_footer() ?>
