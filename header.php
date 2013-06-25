ce4ba2e94bbc7803e0340ba612465b5194dde867
Miguel Ángel Durán García <miduga@gmail.com>
2013-06-25 23:38:07 +0200

    Updated jquery to jQuery 1.10.1

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <meta charset='utf-8'>
-        <title><?php bloginfo('name'); ?> - <?php wp_title(); ?></title>      
+        <title><?php bloginfo('name') ?> - <?php wp_title() ?></title>      
         <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" media="screen" />
         <!--[if lt IE 9]>
             <script src="<?= get_template_directory_uri(); ?>/js/html5shiv.js"></script>
@@ -10,6 +10,6 @@
         <?php wp_head() ?>
     
     <body>
-		<a href="<?php echo get_option('home'); ?>"><?php bloginfo('name') ?></a>
+		<a href="<?= get_option( 'home' ) ?>"><?php bloginfo( 'name' ) ?></a>
 
 		<section id="content">
