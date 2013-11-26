f0c32711820133ee934553de6ad6faa40ab38127
Miguel Ángel Durán García <miduga@gmail.com>
2013-11-26 17:23:23 +0100

    Remove not compatible code for WP3.7

--- functions.php
+++ functions.php
@@ -26,8 +26,3 @@ if ( function_exists('register_sidebar') )
         'after_widget' => ''
     )
 );
-
-// Remove Wordpress and Plugins updates
-add_filter( 'pre_site_transient_update_core', create_function( '$a', "return null;" ) );
-remove_action( 'load-update-core.php', 'wp_update_plugins' );
-add_filter( 'pre_site_transient_update_plugins', create_function( '$a', "return null;" ) );s
