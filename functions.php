9b7a0afe8af7107897cb27ea5bd3943b3654e50a
Miguel Ángel <miguel@miduga.es>
2016-03-10 21:11:57 +0100

    Some tweaks on funcitons and update html5shiv

--- functions.php
+++ functions.php
@@ -1,22 +1,51 @@
 <?
 
-/* 
+/*
 * Remove actions
 * Add a // to the start of the line to comment and enable the action by default
 */
 remove_action('wp_head', 'wp_shortlink_wp_head');
-//remove_action('wp_head', 'index_rel_link');
-//remove_action('wp_head', 'start_post_rel_link');
-//remove_action('wp_head', 'adjacent_posts_rel_link_wp_head');
 remove_action('wp_head', 'rsd_link');
 remove_action('wp_head', 'wlwmanifest_link');
 remove_action('wp_head', 'wp_generator');
 remove_filter( 'the_content', 'capital_P_dangit' );
 remove_filter( 'the_title', 'capital_P_dangit' );
 remove_filter( 'comment_text', 'capital_P_dangit' );
+remove_filter('atom_service_url','atom_service_url_filter');
+//remove_action('wp_head', 'index_rel_link');
+//remove_action('wp_head', 'start_post_rel_link');
+//remove_action('wp_head', 'adjacent_posts_rel_link_wp_head');
 //remove_filter('the_content', 'wptexturize');
 //remove_filter('comment_text', 'wptexturize');
-remove_filter('atom_service_url','atom_service_url_filter');
+
+/* remove inline recent comments */
+// <style type="text/css">.recentcomments a{display:inline !important;padding:0 !important;margin:0 !important;}</style> added in the header
+function remove_recent_comment_style() {
+	global $wp_widget_factory;
+	remove_action(
+    'wp_head',
+    array( $wp_widget_factory->widgets['WP_Widget_Recent_Comments'], 'recent_comments_style' )
+  );
+}
+add_action( 'widgets_init', 'remove_recent_comment_style' );
+
+/* disable feeds and rss */
+$feedDisabled = true;
+if ($feedDisabled) :
+
+  function disableFeed () {
+    wp_die( __('No feed available,please visit our <a href="'. get_bloginfo('url') .'">homepage</a>!') );
+  }
+
+  add_action('do_feed', 'fb_disable_feed', 1);
+  add_action('do_feed_rdf', 'fb_disable_feed', 1);
+  add_action('do_feed_rss', 'fb_disable_feed', 1);
+  add_action('do_feed_rss2', 'fb_disable_feed', 1);
+  add_action('do_feed_atom', 'fb_disable_feed', 1);
+  add_action('do_feed_rss2_comments', 'fb_disable_feed', 1);
+  add_action('do_feed_atom_comments', 'fb_disable_feed', 1);
+endif;
+
 
 if ( function_exists('register_sidebar') )
   register_sidebar(array(
