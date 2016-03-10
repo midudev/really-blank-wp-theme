c779e3121ea54104237c9918ae635c45570064d4
Miguel Ángel <miguel@miduga.es>
2016-03-10 21:21:25 +0100

    Fixing tabs

--- single.php
+++ single.php
@@ -1,13 +1,15 @@
 <? get_header() ?>
 
-	<? if ( have_posts() ) : the_post() ?>
+  <? if ( have_posts() ) : the_post() ?>
 
     <article class="article" itemscope="" itemtype="http://schema.org/BlogPosting" role="article">
 
-     	<h1 class="article_title" itemprop="name"><? the_title(); ?></h1>
+      <h1 class="article_title" itemprop="name"><? the_title(); ?></h1>
 
       <header class="article_meta">
-        <time datetime="<? the_date() ?>" itemprop="datePublished"><? date_post() ?></time>
+        <time datetime="<? the_date() ?>" itemprop="datePublished">
+          <? date_post() ?>
+        </time>
         <span itemprop="articleSection"><? the_tags( '', ' · ', '' ) ?></span>
       </header>
 
@@ -17,8 +19,7 @@
 
     </article>
 
-    <? endif; ?>
+  <? endif ?>
 
 <? get_sidebar() ?>
-
 <? get_footer() ?>
