a0050eb6fdc07c410141d559df03f7e35aa56f21
Miguel Ángel <miguel@miduga.es>
2016-03-10 21:39:31 +0100

    Async and defer scripts. Add menu support.

--- footer.php
+++ footer.php
@@ -4,6 +4,6 @@
 
 </main>
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
-<script src="<?= get_template_directory_uri() ?>/js/functions.js"></script>
+<script defer src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+<script defer src="<?= get_template_directory_uri() ?>/js/functions.js"></script>
 <? wp_footer() ?>
--- functions.php
+++ functions.php
@@ -77,15 +77,16 @@ function date_post() {
 if (!isset($content_width))
   $content_width = 900;
 
-if (function_exists('add_theme_support'))
-{
-    // Add Menu Support
-    //add_theme_support('menus');
-
-    // Add Thumbnail Theme Support
-    add_theme_support('post-thumbnails');
-    //add_image_size('large', 700, '', true); // Large Thumbnail
-    //add_image_size('medium', 250, '', true); // Medium Thumbnail
-    //add_image_size('small', 120, '', true); // Small Thumbnail
-    //add_image_size('custom-size', 700, 200, true); // Custom Thumbnail Size call using the_post_thumbnail('custom-size');
+/* Add Menu Support */
+add_theme_support('menus');
+function register_my_menu() {
+  register_nav_menu('header-menu',__( 'Header Menu' ));
 }
+add_action( 'init', 'register_my_menu' );
+
+/* Add Thumbnail Theme Support */
+add_theme_support('post-thumbnails');
+//add_image_size('large', 700, '', true); // Large Thumbnail
+//add_image_size('medium', 250, '', true); // Medium Thumbnail
+//add_image_size('small', 120, '', true); // Small Thumbnail
+//add_image_size('custom-size', 700, 200, true); // Custom Thumbnail Size call using the_post_thumbnail('custom-size');
