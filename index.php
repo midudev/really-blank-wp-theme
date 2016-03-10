c64a8c3761afbc52c29652d70dbfb4787df57566
Miguel Ángel <miguel@miduga.es>
2016-03-10 21:22:25 +0100

    Fix tabs.

--- index.php
+++ index.php
@@ -1,16 +1,12 @@
-<? get_header(); ?>
+<? get_header() ?>
 
 <? if (have_posts()) : ?>
 
   <? while (have_posts()) : the_post() ?>
-
-  <article>
-
-  <h2><a href="<? the_permalink() ?>"><? the_title() ?></a></h2>
-  <? the_excerpt() ?>
-
-  </article>
-
+    <article>
+      <h2><a href="<? the_permalink() ?>"><? the_title() ?></a></h2>
+      <? the_excerpt() ?>
+    </article>
   <? endwhile ?>
 
 <? endif ?>
