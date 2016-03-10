a8a7ef38e4abe1bc9606330ecc2f0291597fd9b5
Miguel Ángel <miguel@miduga.es>
2016-03-10 20:59:28 +0100

    normalize instead reset

--- header.php
+++ header.php
@@ -3,11 +3,11 @@
     <head>
       <meta charset='utf-8'>
       <meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0">
-      <title><? wp_title() ?></title>      
-      <link rel="stylesheet" href="<? bloginfo('stylesheet_url'); ?>" media="screen" />
+      <title><? wp_title() ?></title>
+      <link rel="stylesheet" href="<? bloginfo('stylesheet_url') ?>" media="screen" />
       <!--[if lt IE 9]><script src="<?= get_template_directory_uri(); ?>/js/html5shiv.js"></script><![endif]-->
       <? wp_head() ?>
-  
+
   <body>
     <a href="<?= get_option( 'home' ) ?>"><? bloginfo( 'name' ) ?></a>
     <main id="content" role="main">
