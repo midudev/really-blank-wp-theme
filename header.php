2fcd00c3ed00c154a0bff84fc7ad60c19dcd6cdf
Miguel Angel Duran Garcia <miguelangel.duran@scmspain.com>
2016-07-18 16:03:56 +0200

    Remove unneded html5shiv

--- header.php
+++ header.php
@@ -4,7 +4,6 @@
   <meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0">
   <title><? wp_title() ?></title>
   <link rel="stylesheet" href="<? bloginfo('stylesheet_url') ?>" media="screen" />
-  <!--[if lt IE 9]><script src="<?= get_template_directory_uri(); ?>/js/html5shiv.js"></script><![endif]-->
   <? wp_head() ?>
 
 <body>
