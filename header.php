49198e603ce1b2b77717b9e19b105d04f5f62d46
Miguel Ángel Durán <miguel@miduga.es>
2017-11-05 18:20:09 +0100

    Add new info to meta tags on the header

--- header.php
+++ header.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html><html>
 <head>
   <meta charset='utf-8'>
-  <meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <title><? wp_title() ?></title>
   <link rel="stylesheet" href="<? bloginfo('stylesheet_url') ?>" media="screen" />
   <? wp_head() ?>
